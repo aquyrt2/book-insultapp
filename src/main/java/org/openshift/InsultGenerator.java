7734e8e13c9d8cc5b1a868affac7b85741fde843
agidens_hermanCop <herman.cop@agidens.com>
2020-01-28 13:40:26 +0100

    NL vertaling

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,16 +4,12 @@ import java.util.Random;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
-		String vowels = "AEIOU";
-		String article = "an";
+		String words[][] = {{"domme", "luie", "dikke"}, {"gedachtenloze", "bodemloze", "vervelende"}, {"Eikel", "Klootzak", "Zakkenwasser"}};
+		String article = "een";
 		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
 		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
 		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+		return String.format("Jij bent %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
 	}
 
 }
